55c2fb6d3a64b77987bf32962ebefc07629cf09a
Maks Fastovets <fastovezz@gmail.com>
2016-05-29 03:18:00 +0300

    add swagger support, NON-MATURE!
    1) add swagger support to /favlists only;
    2) looks like sbt (or play itself... via activator.. not sure...) allows to access dependencies of "org.webjars" group (which are ui dependencies, js ones and alike...) via "/assets/lib/{asset name including its path}" url
    3) ...so add swagger-ui dir under /public dir of play project and paste original swagger's index.html there and add "/assets/lib/" to all url pointing to assets swagger uses in it index.html
     4) add x-auth-token support to swagger via editing mentioned above index.html
     5) as none of io.swagger, com.wordnik, de.zalando supports play 2.5.x I found and use this workaround to support swagger in play 2.5.x environment: https://github.com/CreditCardsCom/swagger-play

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,26 @@
 package controllers;
 
+import play.libs.ws.WSClient;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import javax.inject.Inject;
+import java.util.concurrent.CompletionStage;
+
 /**
  * @author Maks Fastovets.
  */
-public class Application  extends Controller {
+public class Application extends Controller {
+
+    @Inject
+    WSClient ws;
 
     public Result index1() {
         return ok(index.render());
+    }
+
+    public Result google() {
+        return redirect("http://google.com");
     }
 }
