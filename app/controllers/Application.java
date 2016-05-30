2da0b278f91ae0e5b9cf0d86face72fcb9080e49
Maksym Fastovets <maksym.fastovets@g7consulting.com>
2016-05-30 11:13:03 +0300

    app method to Application controller to make proxy calls to https://api.etadirect.com

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.libs.ws.WSClient;
+import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -13,6 +14,7 @@ import java.util.concurrent.CompletionStage;
  */
 public class Application extends Controller {
 
+    public static final String HTTPS_API_ETADIRECT_COM = "https://api.etadirect.com";
     @Inject
     WSClient ws;
 
@@ -22,5 +24,15 @@ public class Application extends Controller {
 
     public Result google() {
         return redirect("http://google.com");
+    }
+
+    public CompletionStage<Result> proxy(String path) {
+
+        return ws.url(HTTPS_API_ETADIRECT_COM + request().path())
+//                .setHeader(request().getHeader()))
+                .get()
+                .thenApply(response ->
+                        status(response.getStatus(), response.getBody())
+                                .as(response.getHeader("Content-Type")));
     }
 }
