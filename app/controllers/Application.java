55c22be1a8b82dfd6b1e1f6b1f41f2e53cc2feb0
Maksym Fastovets <maksym.fastovets@g7consulting.com>
2016-06-01 15:05:18 +0300

    proxies and api-docs for osfc and movies favlists app.
    1) add proxy call to get osfc swagger.json from oracle's host;
    2) add proxy calls to api.etadirect.com REST API via GET/POST/DELETE/PUT/PATCH methods;
    3) extract api.etadirect.com and osfc swagger.json urls to properties file;
    4) expose /api-docs/movies;
    5) add "Access-Control-Allow-Origin: *" to headers returned by my proxies for osfc swagger.json and osfc REST API
    6) add "we're reactive" banner on the main page (not in /swagger-ui/index.html)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,7 @@
 package controllers;
 
+import play.Configuration;
 import play.libs.ws.WSClient;
-import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -14,9 +14,15 @@ import java.util.concurrent.CompletionStage;
  */
 public class Application extends Controller {
 
-    public static final String HTTPS_API_ETADIRECT_COM = "https://api.etadirect.com";
+    public static final String API_ETADIRECT_HOST = "api.etadirect.host";
+    public static final String OSFC_SWAGGER_JSON = "osfc.swagger.json";
+
     @Inject
     WSClient ws;
+
+
+    @Inject
+    Configuration config;
 
     public Result index1() {
         return ok(index.render());
@@ -28,11 +34,23 @@ public class Application extends Controller {
 
     public CompletionStage<Result> proxy(String path) {
 
-        return ws.url(HTTPS_API_ETADIRECT_COM + request().path())
+        return ws.url(config.getString(API_ETADIRECT_HOST) + request().path())
 //                .setHeader(request().getHeader()))
                 .get()
                 .thenApply(response ->
                         status(response.getStatus(), response.getBody())
+                                .withHeader("Access-Control-Allow-Origin", "*")
+                                .as(response.getHeader("Content-Type")));
+    }
+
+    public CompletionStage<Result> osfcApi() {
+        return ws.url(config.getString(OSFC_SWAGGER_JSON))
+                .setMethod(request().method())
+                .execute() // should be used with #setMethod()
+                .thenApply(response ->
+                        status(response.getStatus(), response.getBody())
+//                                .withHeader("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization")
+                                .withHeader("Access-Control-Allow-Origin", "*")
                                 .as(response.getHeader("Content-Type")));
     }
 }
